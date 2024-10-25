9b4f17108ecf60612f0d65a1ba65d8d0b2dbfe13
Mikhail <hudih@mail.ru>
2024-10-25 19:28:58 +0300

    added boost

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
+
 #include "ufo_torrent.h"
+
+#include <boost/any.hpp>
+#include <boost/multiprecision/cpp_bin_float.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <iomanip>
+#include <iostream>
+#include <ncurses.h>
+#include <string.h>
 
 int main()
 {
-	std::cout << testSum(4, 4);
+    using LongFloat = boost::multiprecision::cpp_bin_float_quad;
+
+    const auto x = boost::multiprecision::int128_t(1234123521);
+    const auto y = LongFloat(34532.52346246234);
+    const auto z = LongFloat(x) / y;
+    std::cout << "Ratio: " << std::setprecision(10) << z << "\n";
 }
