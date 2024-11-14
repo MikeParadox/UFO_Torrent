e85106d9545f192284773666bc4ef98b2f56a440
Komrad1991 <likutindan@gmail.com>
2024-11-14 21:19:26 +0300

    simple menu example

--- src/main.cpp
+++ src/main.cpp
@@ -12,35 +12,96 @@
 #include "bencode/prettyPrinter.h"
 
 using namespace bencode;
+#define WIDTH 30
+#define HEIGHT 10 
+
+int startx = 0;
+int starty = 0;
+
+char* choices[] = {
+			const_cast<char*>("Choice 1"),
+			const_cast<char*>("Choice 2"),
+			const_cast<char*>("Choice 3"),
+			const_cast<char*>("Choice 4"),
+			const_cast<char*>("Exit"),
+};
+int n_choices = sizeof(choices) / sizeof(char*);
+void print_menu(WINDOW* menu_win, int highlight);
 
 int main()
 {
-	int ch;
+	WINDOW* menu_win;
+	int highlight = 1;
+	int choice = 0;
+	int c;
 
-	initscr();			/* Start curses mode 		*/
-	raw();				/* Line buffering disabled	*/
-	keypad(stdscr, TRUE);		/* We get F1, F2 etc..		*/
-	noecho();			/* Don't echo() while we do getch */
+	initscr();
+	clear();
+	noecho();
+	cbreak();	/* Line buffering disabled. pass on everything */
+	startx = (80 - WIDTH) / 2;
+	starty = (24 - HEIGHT) / 2;
 
-	printw("Type any character to see it in bold\n");
-	ch = getch();			/* If raw() hadn't been called
-					 * we have to press enter before it
-					 * gets to the program 		*/
-	if (ch == KEY_F(1))		/* Without keypad enabled this will */
-		printw("F1 Key pressed");/*  not get to us either	*/
-	/* Without noecho() some ugly escape
-	 * charachters might have been printed
-	 * on screen			*/
-	else
+	menu_win = newwin(HEIGHT, WIDTH, starty, startx);
+	keypad(menu_win, TRUE);
+	mvprintw(0, 0, "Use arrow keys to go up and down, Press enter to select a choice");
+	refresh();
+	print_menu(menu_win, highlight);
+	while (1)
 	{
-		printw("The pressed key is ");
-		attron(A_BOLD);
-		printw("%c", ch);
-		attroff(A_BOLD);
+		c = wgetch(menu_win);
+		switch (c)
+		{
+		case KEY_UP:
+			if (highlight == 1)
+				highlight = n_choices;
+			else
+				--highlight;
+			break;
+		case KEY_DOWN:
+			if (highlight == n_choices)
+				highlight = 1;
+			else
+				++highlight;
+			break;
+		case 10:
+			choice = highlight;
+			break;
+		default:
+			mvprintw(24, 0, "Charcter pressed is = %3d Hopefully it can be printed as '%c'", c, c);
+			refresh();
+			break;
+		}
+		print_menu(menu_win, highlight);
+		if (choice != 0)	/* User did a choice come out of the infinite loop */
+			break;
 	}
-	refresh();			/* Print it on to the real screen */
-	getch();			/* Wait for user input */
-	endwin();			/* End curses mode		  */
-
+	mvprintw(23, 0, "You chose choice %d with choice string %s\n", choice, choices[choice - 1]);
+	clrtoeol();
+	refresh();
+	endwin();
 	return 0;
+}
+
+
+void print_menu(WINDOW* menu_win, int highlight)
+{
+	int x, y, i;
+
+	x = 2;
+	y = 2;
+	box(menu_win, 0, 0);
+	for (i = 0; i < n_choices; ++i)
+	{
+		if (highlight == i + 1) /* High light the present choice */
+		{
+			wattron(menu_win, A_REVERSE);
+			mvwprintw(menu_win, y, x, "%s", choices[i]);
+			wattroff(menu_win, A_REVERSE);
+		}
+		else
+			mvwprintw(menu_win, y, x, "%s", choices[i]);
+		++y;
+	}
+	wrefresh(menu_win);
 }
