c33492a2c8a63908f40686a8942e48def7f6a962
Mikhail <hudih@mail.ru>
2024-11-28 23:20:40 +0300

    minor cmake changes

---
+++ build/tests/main_tests[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/home/mikhail/programming/c++/projects/UFO-Torrent/build/tests/main_tests[1]_tests.cmake")
+  include("/home/mikhail/programming/c++/projects/UFO-Torrent/build/tests/main_tests[1]_tests.cmake")
+else()
+  add_test(main_tests_NOT_BUILT main_tests_NOT_BUILT)
+endif()
---
+++ build/tests/main_tests[1]_tests.cmake
@@ -0,0 +1,1 @@
+set(  main_tests_TESTS)
