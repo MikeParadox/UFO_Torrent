7bcde776837fe3f964ba1be97e3a93fdf58c27d6
Mikhail <hudih@mail.ru>
2025-04-17 01:13:51 +0300

    Cleanup

--- conanfile.py
+++ conanfile.py
@@ -7,28 +7,26 @@ class ExampleRecipe(ConanFile):
     generators = "CMakeDeps", "CMakeToolchain"
 
     default_options = {
-    "libtorrent/*:shared": False,
-    "boost/*:shared": False,
-    "openssl/*:shared": False,  # Required for libtorrent
-    "zlib/*:shared": False,     # Often needed by libcurl/OpenSSL
-    "libcurl/*:shared": False,  # If using libcurl
-    "ncurses/*:shared": False,  # If using ncurses
-    # Add other deps here
-}
+        "libtorrent/*:shared": False,
+        "boost/*:shared": False,
+        "openssl/*:shared": False,  # Required for libtorrent
+        "zlib/*:shared": False,  # Often needed by libcurl/OpenSSL
+        "libcurl/*:shared": False,  # If using libcurl
+        "ncurses/*:shared": False,  # If using ncurses
+        # Add other deps here
+    }
 
     def requirements(self):
         self.requires("libtorrent/2.0.10")
         self.requires("gtest/1.15.0")
         self.requires("ncurses/6.5")
         self.requires("libcurl/8.10.1")
-        self.requires("boost/1.83.0",override=True)
-
-   
+        self.requires("boost/1.83.0", override=True)
 
     def layout(self):
         cmake_layout(self)
 
-    def build(self):
-        self.run(
-            f"conan install . --build=missing -s compiler.cppstd=17 -s build_type=Release -c tools.build:defines='BOOST_CSTDFLOAT_NO_LIBSTDCXX_FLOAT128'"
-        )
+    # def build(self):
+    #     self.run(
+    #         f"conan install . --build=missing  -c tools.build:defines='BOOST_CSTDFLOAT_NO_LIBSTDCXX_FLOAT128'"
+    #     )
