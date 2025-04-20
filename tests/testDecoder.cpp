8c7dc851a1aebd2f9236999c66d9f9e527344998
Боднар Виталий <Lyl1.32@yandex.ru>
2025-04-21 00:13:42 +0300

    Added fix for Encoder test and comments to them(+ Decoder)

--- tests/testDecoder.cpp
+++ tests/testDecoder.cpp
@@ -1,6 +1,5 @@
 #include <gtest/gtest.h>
 #include "../bencode/Decoder.h"
-#include "../bencode/Decoder.h"
 #include "../bencode/BInteger.h"  
 #include "../bencode/BItem.h"  
 
@@ -14,23 +13,24 @@ namespace bencoding {
         }
     };
     TEST_F(DecoderTest, DictionarySorting) {
-    //    // Valid (sorted keys)
+        // Test valid dictionary decoding
         auto result = decoder->decode("d1:a1:b2:aa1:ce");
         ASSERT_NE(result, nullptr);
 
-        // Invalid text
+        // Test invalid dictionary format
         ASSERT_THROW(decoder->decode("dinvalid"), DecodingError);
     }
     TEST_F(DecoderTest, Integers)
     {
+        // First verify decoder was created successfully
         ASSERT_NE(decoder, nullptr) << "Decoder creation failed";
-
+        // Test decoding of valid integer "i42e" (bencoded 42)
         auto decoded_item = decoder->decode("i42e");
         ASSERT_NE(decoded_item, nullptr) << "Decoding failed";
-
+        // Try to cast decoded item to BInteger*
         BInteger* intr = dynamic_cast<BInteger*>(decoded_item.get());
         ASSERT_NE(intr, nullptr) << "Not a BInteger object" << typeid(*decoded_item).name();
- 
+        // Verify the integer value matches expected 42
         ASSERT_EQ(intr->value(), 42) << "Wrong integer value";
     }
 }
--- tests/testEncoder.cpp
+++ tests/testEncoder.cpp
@@ -1,12 +1,12 @@
 #include <gtest/gtest.h>
-#include "../src/Encoder.h"
-#include "../src/BInteger.h"
-#include "../src/BString.h"
-#include "../src/BList.h"
-#include "../src/BDictionary.h"
+#include "../bencode/Encoder.h"
+#include "../bencode/BInteger.h"
+#include "../bencode/BString.h"
+#include "../bencode/BList.h"
+#include "../bencode/BDictionary.h"
 
-namespace bencoding;
-namespace {
+
+namespace bencoding {
 
 	class EncoderTest : public ::testing::Test {
 	protected:
@@ -17,53 +17,47 @@ namespace {
 		std::unique_ptr<bencoding::Encoder> encoder;
 	};
 
-	TEST_F(EncoderTest, EncodesPositiveInteger) {
-		auto integer = bencoding::BInteger::create(42);
-		ASSERT_EQ(encoder->encode(integer), "i42e");
+	TEST_F(EncoderTest, PositiveInteger) {
+		// Test basic positive integer encoding
+		auto integer = BInteger::create(42);
+		std::string result = encoder->encode(std::shared_ptr<BItem>(integer.release()));
+		ASSERT_EQ(result, "i42e") << "Positive integer encoding failed";
 	}
 
-	TEST_F(EncoderTest, EncodesNegativeInteger) {
-		auto integer = bencoding::BInteger::create(-10);
-		ASSERT_EQ(encoder->encode(integer), "i-10e");
+	// Test negative integer encoding
+	TEST_F(EncoderTest, NegativeInteger) {
+		auto integer = BInteger::create(-123);
+		std::string result = encoder->encode(std::shared_ptr<BItem>(integer.release()));
+		ASSERT_EQ(result, "i-123e") << "Negative integer encoding failed";
 	}
 
-	TEST_F(EncoderTest, EncodesZero) {
-		auto integer = bencoding::BInteger::create(0);
-		ASSERT_EQ(encoder->encode(integer), "i0e");
+	// Test empty string encoding
+	TEST_F(EncoderTest, EmptyString) {
+		auto str = BString::create("");
+		std::string result = encoder->encode(std::shared_ptr<BItem>(str.release()));
+		ASSERT_EQ(result, "0:") << "Empty string encoding failed";
 	}
 
-	TEST_F(EncoderTest, EncodesRegularString) {
-		auto str = bencoding::BString::create("test");
-		ASSERT_EQ(encoder->encode(str), "4:test");
+	// Test non-empty string encoding
+	TEST_F(EncoderTest, NonEmptyString) {
+		auto str = BString::create("test");
+		std::string result = encoder->encode(std::shared_ptr<BItem>(str.release()));
+		ASSERT_EQ(result, "4:test") << "Non-empty string encoding failed";
 	}
 
-	TEST_F(EncoderTest, EncodesEmptyString) {
-		auto str = bencoding::BString::create("");
-		ASSERT_EQ(encoder->encode(str), "0:");
+	// Test list with multiple elements encoding
+	TEST_F(EncoderTest, NonEmptyList) {
+		auto list = BList::create();
+		list->push_back(BInteger::create(1));
+		list->push_back(BString::create("abc"));
+		std::string result = encoder->encode(std::shared_ptr<BItem>(list.release()));
+		ASSERT_EQ(result, "li1e3:abce") << "Non-empty list encoding failed";
 	}
 
-	TEST_F(EncoderTest, EncodesSimpleList) {
-		auto list = bencoding::BList::create();
-		list->push_back(bencoding::BInteger::create(42));
-		list->push_back(bencoding::BString::create("foo"));
-		ASSERT_EQ(encoder->encode(list), "li42e3:fooe");
+	// Test empty dictionary encoding
+	TEST_F(EncoderTest, EmptyDictionary) {
+		auto dict = BDictionary::create();
+		std::string result = encoder->encode(std::shared_ptr<BItem>(dict.release()));
+		ASSERT_EQ(result, "de") << "Empty dictionary encoding failed";
 	}
-
-	TEST_F(EncoderTest, EncodesSimpleDictionary) {
-		auto dict = bencoding::BDictionary::create();
-		dict->insert(bencoding::BString::create("foo"), bencoding::BString::create("bar"));
-		dict->insert(bencoding::BString::create("num"), bencoding::BInteger::create(42));
-		ASSERT_EQ(encoder->encode(dict), "d3:foo3:bar3:numi42ee");
-	}
-
-	TEST_F(EncoderTest, EncodesNestedStructures) {
-		auto inner_list = bencoding::BList::create();
-		inner_list->push_back(bencoding::BString::create("hello"));
-
-		auto dict = bencoding::BDictionary::create();
-		dict->insert(bencoding::BString::create("list"), inner_list);
-
-		ASSERT_EQ(encoder->encode(dict), "d4:listl5:helloee");
-	}
-
 } 
