5b019ac0479552a9cdd610c9fa2006ce1ed4433f
Mikhail <hudih@mail.ru>
2025-04-27 19:16:43 +0300

    Fixed compatibility with Ubuntu 22.04 and removed .deb package

--- src/network_part/connect.cpp
+++ src/network_part/connect.cpp
@@ -10,10 +10,12 @@
 #include <stdexcept>
 #include <sys/poll.h>
 #include <sys/socket.h>
+#include <vector>
+#include <string>
 #include <unistd.h>
 
-constexpr int connection_timeout{ 10 }; // ���� 3
-constexpr int read_timeout{ 10000 };
+constexpr int connection_timeout{10}; // ���� 3
+constexpr int read_timeout{10000};
 
 // Устанавливаем сокет в блокирующий/неблокирующий режим
 bool setSocketBlocking(int sock, bool blocking)
@@ -33,12 +35,13 @@ int createConnection(const std::string& ip, const int port)
     int sock = 0;
     struct sockaddr_in address;
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-        throw std::runtime_error("Socket creation error: " + std::to_string(sock));
+        throw std::runtime_error(
+            "Socket creation error: " + std::to_string(sock));
 
     address.sin_family = AF_INET;
     address.sin_port = htons(port);
 
-    char* tempIp{ new char[ip.length() + 1] };
+    char* tempIp{new char[ip.length() + 1]};
     strcpy(tempIp, ip.c_str());
 
     // Преобразуем IP-адрес из строки
@@ -48,7 +51,7 @@ int createConnection(const std::string& ip, const int port)
     // Ставим сокет в неблокирующий режим для connect
     if (!setSocketBlocking(sock, false))
         throw std::runtime_error("An error occurred when setting socket "
-            + std::to_string(sock) + " to NONBLOCK");
+                                 + std::to_string(sock) + " to NONBLOCK");
 
     connect(sock, (struct sockaddr*)&address, sizeof(address));
 
@@ -63,14 +66,15 @@ int createConnection(const std::string& ip, const int port)
     if (select(sock + 1, NULL, &fdset, NULL, &tv) == 1)
     {
         int so_error;
-        socklen_t len{ sizeof so_error };
+        socklen_t len{sizeof so_error};
         getsockopt(sock, SOL_SOCKET, SO_ERROR, &so_error, &len);
 
         if (so_error == 0)
         {
             // Переключаемся обратно в блокирующий режим
             if (!setSocketBlocking(sock, true))
-                throw std::runtime_error("An error occurred when setting socket "
+                throw std::runtime_error(
+                    "An error occurred when setting socket "
                     + std::to_string(sock) + " to BLOCK");
             return sock;
         }
@@ -78,7 +82,7 @@ int createConnection(const std::string& ip, const int port)
 
     close(sock);
     throw std::runtime_error("Connect to " + ip + ":" + std::to_string(port)
-        + ": FAILED [Connection timeout]");
+                             + ": FAILED [Connection timeout]");
 }
 
 // Отправка данных пиру
@@ -89,39 +93,46 @@ void sendData(const int sock, const std::string& data)
     int res = send(sock, data.data(), n, 0);
     if (res < 0)
         throw std::runtime_error("Failed to write data to socket "
-            + std::to_string(sock));
+                                 + std::to_string(sock));
 }
 
 // Корректное чтение данных BitTorrent-сообщений
 std::string receiveData(int sock, int expected_size)
 {
-    if (sock <= 0) {
+    if (sock <= 0)
+    {
         throw std::runtime_error("Invalid socket");
     }
 
     // Если размер не задан явно, значит сначала берем префикс длины
-    if (expected_size == 0) {
+    if (expected_size == 0)
+    {
         uint32_t length_prefix = 0;
         ssize_t bytes_received = recv(sock, &length_prefix, 4, 0);
 
-        if (bytes_received <= 0) {
-            if (bytes_received == 0) {
+        if (bytes_received <= 0)
+        {
+            if (bytes_received == 0)
+            {
                 throw std::runtime_error("Connection closed by peer");
             }
-            else {
+            else
+            {
                 throw std::runtime_error("Failed to receive data: "
-                    + std::string(strerror(errno)));
+                                         + std::string(strerror(errno)));
             }
         }
 
-        if (bytes_received < 4) {
+        if (bytes_received < 4)
+        {
             throw std::runtime_error("Incomplete length prefix received");
         }
 
         length_prefix = ntohl(length_prefix); // big-endian -> host order
 
         // Если ноль, то это keep-alive
-        if (length_prefix == 0) {
+        if (length_prefix == 0)
+        {
             return "";
         }
         expected_size = length_prefix;
@@ -131,15 +142,21 @@ std::string receiveData(int sock, int expected_size)
     ssize_t total_bytes = 0;
 
     // Считываем весь ожидаемый объем данных по частям
-    while (total_bytes < expected_size) {
+    while (total_bytes < expected_size)
+    {
         ssize_t bytes = recv(sock, buffer.data() + total_bytes,
-            expected_size - total_bytes, 0);
-        if (bytes <= 0) {
-            if (bytes == 0) {
-                throw std::runtime_error("Connection closed by peer during message receive");
+                             expected_size - total_bytes, 0);
+        if (bytes <= 0)
+        {
+            if (bytes == 0)
+            {
+                throw std::runtime_error(
+                    "Connection closed by peer during message receive");
             }
-            else {
-                throw std::runtime_error("Error receiving data: " + std::string(strerror(errno)));
+            else
+            {
+                throw std::runtime_error(
+                    "Error receiving data: " + std::string(strerror(errno)));
             }
         }
         total_bytes += bytes;
